72c60ccd782f074650005d1fbb1c2c5a4b6e9dc4
jarle pedersen <jarle.pedersen@norconsult.com>
2016-09-07 11:06:58 +0200

    generate urls based on input. prints to console when input.length > 3

--- src/app/components/transclusions/searchBar/searchBar.directive.js
+++ src/app/components/transclusions/searchBar/searchBar.directive.js
@@ -1,15 +1,18 @@
 angular.module('searchBar')
-    .directive('searchBar', [
-        function() {
+    .directive('searchBar', ['mainAppService',
+        function(mainAppService) {
             return {
                 templateUrl: 'components/transclusions/searchBar/searchBar.html',
                 transclude: true,
                 restrict: 'A',
                 link: function(scope){
-
-
-                    scope.testClick = function () {
-                        console.log("test click clicked");
+                    scope.searchBarValueChanged = function () {
+                        var query=scope.searchBarModel + '';
+                        console.log("searchBar Value Changed to " + query);
+                        if (query.length < 4){ return;}
+                        console.log(mainAppService.generateSearchVegUrl(query));
+                        console.log(mainAppService.generateSearchAdresseUrl(query));
+                        console.log(mainAppService.generateSearchStedsnavnUrl(query));
                     };
                 }
             };
