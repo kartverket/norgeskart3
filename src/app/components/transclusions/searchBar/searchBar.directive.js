bea167b8cc1992ee49a456cd180548cb831d947f
jarle pedersen <jarle.pedersen@norconsult.com>
2016-09-07 11:54:37 +0200

    restructured functions. added download

--- src/app/components/transclusions/searchBar/searchBar.directive.js
+++ src/app/components/transclusions/searchBar/searchBar.directive.js
@@ -1,18 +1,52 @@
 angular.module('searchBar')
     .directive('searchBar', ['mainAppService',
         function(mainAppService) {
+
+        var _getQuery =  function(scope) {
+                return scope.searchBarModel + '';
+        };
+
+        var _generateUrls = function(query){
+            return [
+                mainAppService.generateSearchVegUrl(query),
+                mainAppService.generateSearchAdresseUrl(query),
+                mainAppService.generateSearchStedsnavnUrl(query)
+            ];
+        };
+
+        var _getResults = function(query){
+            var urls=_generateUrls(query);
+            for (var urlIndex = 0; urlIndex < urls.length; urlIndex++){
+                //console.log(urls[urlIndex]);
+                _downloadFromUrl(urls[urlIndex]);
+            }
+        };
+
+        var _downloadFromUrl = function (url) {
+                $.ajax({
+                    type: "GET",
+                    url: url,
+                    async: true,
+                    success: function (searchResult) {
+                        xmlFile = searchResult;
+                        //console.log(xmlFile);
+                    },
+                    error: function (searchError) {
+                        console.log("Error load xml file: ", searchError);
+                    }
+                });
+            };
+
             return {
                 templateUrl: 'components/transclusions/searchBar/searchBar.html',
                 transclude: true,
                 restrict: 'A',
                 link: function(scope){
                     scope.searchBarValueChanged = function () {
-                        var query=scope.searchBarModel + '';
-                        console.log("searchBar Value Changed to " + query);
+                        var query=_getQuery(scope);
+                        //console.log("searchBar Value Changed to " + query);
                         if (query.length < 4){ return;}
-                        console.log(mainAppService.generateSearchVegUrl(query));
-                        console.log(mainAppService.generateSearchAdresseUrl(query));
-                        console.log(mainAppService.generateSearchStedsnavnUrl(query));
+                        _getResults(query);
                     };
                 }
             };
