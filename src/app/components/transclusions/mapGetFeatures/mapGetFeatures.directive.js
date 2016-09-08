279dbd9b9779369cb8ff7fa26cf0284f5beadf72
jucir <juraj.cirbus@norconsutl.com>
2016-09-08 14:19:17 +0200

    get features

--- src/app/components/transclusions/mapGetFeatures/mapGetFeatures.directive.js
+++ src/app/components/transclusions/mapGetFeatures/mapGetFeatures.directive.js
@@ -1,11 +1,75 @@
 angular.module('mapGetFeatures')
-    .directive('mapGetFeatures', [
-        function() {
+    .directive('mapGetFeatures', ['ISY.EventHandler',
+        function(eventHandler) {
             return {
                 templateUrl: 'components/transclusions/mapGetFeatures/mapGetFeatures.html',
-                transclude: true,
                 restrict: 'A',
-                link: function(){
+                link: function(scope){
+
+                    scope.layers = [];
+
+                    function _handleLoadingLayers(loadingLayers){
+                        for(var i = 0; i < loadingLayers.length; i++){
+                            // console.log(loadingLayers[i]);
+                            _addLoadingLayer(loadingLayers[i]);
+                        }
+                    }
+
+                    function _getLoadingLayer(id){
+                        for(var i = 0; i < scope.layers.length; i++){
+                            var loadingLayer = scope.layers[i];
+                            if(loadingLayer.id === id){
+                                return loadingLayer;
+                            }
+                        }
+                        return null;
+                    }
+
+                    function _addLoadingLayer(loadingLayer) {
+                        scope.layers.push(loadingLayer);
+                    }
+
+                    function _loadResult(resultSet) {
+                        var loadingLayer = _getLoadingLayer(resultSet.id);
+                        if (loadingLayer !== null){
+                            if(resultSet.exception){
+                                loadingLayer.exception = resultSet.exception;
+                                loadingLayer.hasException = true;
+                            }
+                            else {
+                                if (resultSet.features !== undefined){
+                                    if(resultSet.features.length > 0) {
+
+                                        //if (resultSet.features[0].attributes.length > 0){
+                                        loadingLayer.features = resultSet.features;
+                                        // loadingLayer.featureInfo = resultSet.featureInfoElement;
+                                        // loadingLayer.featureInfoTitle = resultSet.featureInfoTitle;
+                                        // loadingLayer.wms = resultSet.wms;
+                                        loadingLayer.hasFeatures = resultSet.showDialog;
+                                        // loadingLayer.hasFeatureInfoDetail = findFeatureInfoDetail(resultSet.featureInfoElement);
+                                        // loadingLayer.openNewWindow = resultSet.openNewWindow;
+                                        // loadingLayer.openParentWindow = resultSet.openParentWindow;
+                                        // loadingLayer.windowWidth = resultSet.windowWidth;
+                                        // loadingLayer.editable = resultSet.editable;
+                                        // loadingLayer.attachment = resultSet.attachment;
+                                        //}else if (resultSet.editable !== undefined){
+                                        //    loadingLayer.editable = resultSet.editable;
+                                        //}
+                                    }
+                                }
+                            }
+
+                            if(loadingLayer.hasFeatures){
+                                loadingLayer.show = true;
+                            }
+
+                            loadingLayer.isLoading = false;
+                        }
+                        console.log(resultSet);
+                    }
+
+                    eventHandler.RegisterEvent(ISY.Events.EventTypes.FeatureInfoStart, _handleLoadingLayers);
+                    eventHandler.RegisterEvent(ISY.Events.EventTypes.FeatureInfoEnd, _loadResult);
 
                 }
             };
--- src/app/mainApp.directive.js
+++ src/app/mainApp.directive.js
@@ -3,18 +3,28 @@ angular.module('mainApp')
         function(toolsFactory) {
             return {
                 templateUrl: 'mainAppBody.html',
-                link: function () {
+                link: function (scope, element) {
+                    if (element.scope){
+                        element = $(element);
+                    }
 
                     var addFeatureTool = toolsFactory.getToolById("AddLayerFeature");
                     var measureLine = toolsFactory.getToolById("MeasureLine");
                     var measure = toolsFactory.getToolById("Measure");
+                    var pointSelectTool = toolsFactory.getToolById('PointSelect');
 
                     $(document).keyup(function(e){
                         if (e.keyCode == 27){
                             if (addFeatureTool.isSelected || measureLine.isSelected || measure.isSelected){
                                 toolsFactory.deactivatAllTools();
-                                toolsFactory.setSelectTool('PointSelect');
+                                toolsFactory.activateTool(pointSelectTool);
                             }
+                        }
+                    });
+
+                    element.on('mousedown touchstart', function () {
+                        if (!addFeatureTool.isSelected && !measureLine.isSelected && !measure.isSelected){
+                            toolsFactory.activateTool(pointSelectTool);
                         }
                     });
 
