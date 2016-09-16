94e4e7c8c9c4b320a1263f7204b01dd9df69a45b
jarle pedersen <jarle.pedersen@norconsult.com>
2016-09-16 14:11:42 +0200

    added pointer-cursor class to searchOptions. moved searchOptions-dict to array in function

--- src/app/components/transclusions/searchPanel/searchOptionsPanel/searchOptionsPanel.controller.js
+++ src/app/components/transclusions/searchPanel/searchOptionsPanel/searchOptionsPanel.controller.js
@@ -1,36 +1,44 @@
 angular.module('searchOptionsPanel')
     .controller('searchOptionsPanelController', ['$scope',
         function($scope){
-            $scope.searchOptions = {};
-            $scope.searchOptions['seEiendom'] = {
-                icon: '🏠',
-                text: 'Se eiendom',
-                name: 'seEiendom'
-            };
-            $scope.searchOptions['ssrFakta'] = {
-                icon: '⚑',
-                text: 'Se fakta om stedsnavnet',
-                name: 'ssrFakta'
-            };
-            $scope.searchOptions['turKart'] = {
-                icon: '🚶',
-                text: 'Lage turkart',
-                name: 'turKart'
-            };
-            $scope.searchOptions['nødplakat'] = {
-                icon: '🚑',
-                text: 'Lage nødplakat',
-                name: 'nødplakat'
-            };
-            $scope.searchOptions['seHavnivå'] = {
-                icon: '🌊',
-                text: 'Se havnivå',
-                name: 'seHavnivå'
-            };
-            $scope.searchOptions['seKoordinater'] = {
-                icon: 'x,y',
-                text: 'Se koordinater',
-                name: 'seKoordinater'
-            };
+
+            var _initSearchOptions= function() {
+
+                $scope.searchOptions = [
+                {
+                    icon: '🏠',
+                    text: 'Se eiendom',
+                    name: 'seEiendom'
+                },
+                {
+                    icon: '⚑',
+                    text: 'Se fakta om stedsnavnet',
+                    name: 'ssrFakta'
+                },
+                {
+                    icon: '🚶',
+                    text: 'Lage turkart',
+                    name: 'turKart'
+                },
+                {
+                    icon: '🚑',
+                    text: 'Lage nødplakat',
+                    name: 'nødplakat'
+                },
+                {
+                    icon: '🌊',
+                    text: 'Se havnivå',
+                    name: 'seHavnivå'
+                },
+                {
+                    icon: 'x,y',
+                    text: 'Se koordinater',
+                    name: 'seKoordinater'
+                }
+
+                ];};
+
+                _initSearchOptions();
+
         }
     ]);
