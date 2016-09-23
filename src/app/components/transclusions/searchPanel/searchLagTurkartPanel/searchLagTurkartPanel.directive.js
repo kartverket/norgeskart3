32183e189ae28bf99c167265e7f3d596c5016e87
jarle pedersen <jarle.pedersen@norconsult.com>
2016-09-23 10:49:33 +0200

    printBoxSelect registered as tool

--- src/app/components/transclusions/searchPanel/searchLagTurkartPanel/searchLagTurkartPanel.directive.js
+++ src/app/components/transclusions/searchPanel/searchLagTurkartPanel/searchLagTurkartPanel.directive.js
@@ -1,10 +1,12 @@
 angular.module('searchLagTurkartPanel')
-    .directive('searchLagTurkartPanel', [
-        function() {
+    .directive('searchLagTurkartPanel', ['toolsFactory',
+        function(toolsFactory) {
             return {
                 templateUrl: 'components/transclusions/searchPanel/searchLagTurkartPanel/searchLagTurkartPanel.html',
                 restrict: 'A',
                 link: function(){
+                    var printBoxSelectTool = toolsFactory.getToolById("PrintBoxSelect");
+                    toolsFactory.activateTool(printBoxSelectTool);
                 }
             };
         }]);
