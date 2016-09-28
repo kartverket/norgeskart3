bf5aad869f5692a773227c6b45945cfd61e62a25
jarle pedersen <jarle.pedersen@norconsult.com>
2016-09-28 14:48:04 +0200

    printing (not working due to CORS). refactoring

--- src/app/components/transclusions/searchPanel/searchLagNodplakatPanel/searchLagNodplakatPanelPage3/searchLagNodplakatPanelPage3.controller.js
+++ src/app/components/transclusions/searchPanel/searchLagNodplakatPanel/searchLagNodplakatPanelPage3/searchLagNodplakatPanelPage3.controller.js
@@ -3,16 +3,16 @@ angular.module('searchLagNodplakatPanelPage3')
     function(mainAppService, map, $scope, $sce) {
 
         var _round = function (value, decimals) {
-            return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+            return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
         };
 
         var geographicDecimalToSecondsMinutes = function (geographicOrdinal) {
-            var degrees=geographicOrdinal.toString().split('.')[0];
-            var decimals=geographicOrdinal - degrees;
-            var minutes=decimals * 60;
-            var minutesDecimals=minutes - minutes.toString().split('.')[0];
-            minutes=minutes.toString().split('.')[0];
-            var seconds=minutesDecimals * 60;
+            var degrees = geographicOrdinal.toString().split('.')[0];
+            var decimals = geographicOrdinal - degrees;
+            var minutes = decimals * 60;
+            var minutesDecimals = minutes - minutes.toString().split('.')[0];
+            minutes = minutes.toString().split('.')[0];
+            var seconds = minutesDecimals * 60;
             return {
                 degrees: degrees,
                 minutes: minutes,
@@ -21,19 +21,19 @@ angular.module('searchLagNodplakatPanelPage3')
         };
 
         var geographicalText = function (geographicOrdinal) {
-            var geographicDegreesSecondsMinutes=geographicDecimalToSecondsMinutes(geographicOrdinal);
-            return geographicDegreesSecondsMinutes.degrees + ' grader ' + geographicDegreesSecondsMinutes.minutes + ' minutter ' + _round(geographicDegreesSecondsMinutes.seconds,0) + ' sekunder';
+            var geographicDegreesSecondsMinutes = geographicDecimalToSecondsMinutes(geographicOrdinal);
+            return geographicDegreesSecondsMinutes.degrees + ' grader ' + geographicDegreesSecondsMinutes.minutes + ' minutter ' + _round(geographicDegreesSecondsMinutes.seconds, 0) + ' sekunder';
         };
 
         var emergencyPosterConfig = {
             'locationName': $scope.lagNodplakatName,
             'position1': geographicalText($scope.activePosition.geographicPoint[1]) + ' nord',
             'position2': geographicalText($scope.activePosition.geographicPoint[0]) + ' øst',
-            'street': $scope.lagNodplakatDict.emergencyPosterPoint.veg + ' i ' +  $scope.lagNodplakatDict.emergencyPosterPoint.kommune,
+            'street': $scope.lagNodplakatDict.emergencyPosterPoint.veg + ' i ' + $scope.lagNodplakatDict.emergencyPosterPoint.kommune,
             'place': $scope.lagNodplakatDict.elevationPoint,
             'matrikkel': $scope.lagNodplakatDict.emergencyPosterPoint.matrikkelnr,
-            'utm': 'Sone 33W Ø '+ _round($scope.activePosition.lon,0) +' N ' + _round($scope.activePosition.lat,0),
-            'posDez': 'N' + _round($scope.activePosition.geographicPoint[0],4) + '° - E' + _round($scope.activePosition.geographicPoint[1],4) + '°',
+            'utm': 'Sone 33W Ø ' + _round($scope.activePosition.lon, 0) + ' N ' + _round($scope.activePosition.lat, 0),
+            'posDez': 'N' + _round($scope.activePosition.geographicPoint[0], 4) + '° - E' + _round($scope.activePosition.geographicPoint[1], 4) + '°',
             'map': ''
         };
 
@@ -52,40 +52,11 @@ angular.module('searchLagNodplakatPanelPage3')
         };
 
 
-
-        $scope.generateEmergancyPoster= function () {
-            // map.SetCenter({
-            //     'lon': $scope.activePosition.lon,
-            //     'lat': $scope.activePosition.lat,
-            //     'zoom': 10
-            // });
-
+        $scope.generateEmergancyPoster = function () {
             var extent = map.GetExtent();
             emergencyMapConfig.BBOX = extent[0] + "," + extent[1] + "," + extent[2] + "," + extent[3];
             emergencyPosterConfig.map = mainAppService.generateMapLinkServiceUrl(emergencyMapConfig);
-
-
-            var emergencyPosterServiceUrl=mainAppService.generateEmergencyPosterServiceUrl(emergencyPosterConfig);
-            console.log(emergencyPosterServiceUrl);
+            var emergencyPosterServiceUrl = mainAppService.generateEmergencyPosterServiceUrl(emergencyPosterConfig);
             return $sce.trustAsResourceUrl(emergencyPosterServiceUrl);
         };
-
-
-        // var getEmergencyPosterConfig= function () {
-        //     return emergencyPosterConfig;
-        // };
-        //
-        //
-        // var updateEmergencyPosterConfig= function (config) {
-        //     emergencyPosterConfig = config;
-        // };
-        //
-        //
-        // var setPosterPosition = function (coor) {
-        //     posterPosition = coor;
-        // };
-
-        //generateEmergancyPoster();
-
-
     }]);
--- src/app/components/transclusions/searchPanel/searchLagNodplakatPanel/searchLagNodplakatPanelPage3/searchLagNodplakatPanelPage3.directive.js
+++ src/app/components/transclusions/searchPanel/searchLagNodplakatPanel/searchLagNodplakatPanelPage3/searchLagNodplakatPanelPage3.directive.js
@@ -5,8 +5,12 @@ angular.module('searchLagNodplakatPanelPage3')
                 templateUrl: 'components/transclusions/searchPanel/searchLagNodplakatPanel/searchLagNodplakatPanelPage3/searchLagNodplakatPanelPage3.html',
                 restrict: 'A',
                 controller: 'searchLagNodplakatPanelPage3Controller',
-                link: function(){
-
+                link: function (scope) {
+                    scope.printIframe = function () {
+                        var PDF = document.getElementById('emergencyPoster');
+                        PDF.focus();
+                        PDF.contentWindow.print();
+                    };
                 }
             };
         }]);
