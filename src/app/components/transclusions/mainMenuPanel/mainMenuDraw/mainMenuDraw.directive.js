567c73d6da811920d3804d8ab6b0786b641374e9
jarle pedersen <jarle.pedersen@norconsult.com>
2016-10-07 14:06:30 +0200

    initiate drawing from menu

--- src/app/components/transclusions/mainMenuPanel/mainMenuDraw/mainMenuDraw.directive.js
+++ src/app/components/transclusions/mainMenuPanel/mainMenuDraw/mainMenuDraw.directive.js
@@ -1,12 +1,33 @@
 angular.module('mainMenuDraw')
-    .directive('mainMenuDraw', [
-        function() {
+    .directive('mainMenuDraw', [ 'toolsFactory', 'ISY.EventHandler', //'ISY.MapAPI.Map',
+        function(toolsFactory, eventHandler) {
             return {
                 templateUrl: 'components/transclusions/mainMenuPanel/mainMenuDraw/mainMenuDraw.html',
                 restrict: 'A',
-                link: function(){
+                link: function(scope){
 
+                    /*
+                     Draw start
+                     */
 
+                    scope.drawFeature = function () {
+                        _elevationProfileActive = false;
+                        _emergencyPosterActive = false;
+                        var drawFeatureTool = toolsFactory.getToolById("DrawFeature");
+                        drawFeatureTool.additionalOptions.type='Polygon';
+                        toolsFactory.activateTool(drawFeatureTool);
+                        console.log('Activation invoked');
+                        scope.closeNav();
+                        eventHandler.RegisterEvent(ISY.Events.EventTypes.DrawFeatureEnd, getDrawing);
+                    };
+
+                    var getDrawing = function (geoJSON) {
+                        console.log(geoJSON);
+                    };
+
+                    /*
+                     Draw end
+                     */
 
                 }
             };
