b26df7e45ad17c729d41d420798f0c3249761969
jucir <juraj.cirbus@norconsutl.com>
2016-10-07 13:36:31 +0200

    bug fix

--- src/app/components/transclusions/mainMenuPanel/mainMenuDraw/mainMenuDraw.directive.js
+++ src/app/components/transclusions/mainMenuPanel/mainMenuDraw/mainMenuDraw.directive.js
@@ -4,7 +4,7 @@ angular.module('mainMenuDraw')
             return {
                 templateUrl: 'components/transclusions/mainMenuPanel/mainMenuDraw/mainMenuDraw.html',
                 restrict: 'A',
-                link: function(scope){
+                link: function(){
 
 
 
