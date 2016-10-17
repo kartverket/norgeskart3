296d4ab891efc518a36c0bcb8dd3a7a7e8658b92
jucir <juraj.cirbus@norconsutl.com>
2016-10-17 15:22:44 +0200

    - read & apply parameters from url

--- src/app/mainApp.controller.js
+++ src/app/mainApp.controller.js
@@ -1,6 +1,6 @@
 angular.module('mainApp')
-    .controller('mainAppController', ['$scope','ISY.MapAPI.Map','mainAppFactory','toolsFactory','ISY.EventHandler','isyTranslateFactory','$location','mainMenuPanelFactory', 'localStorageFactory','$translate',
-        function($scope, map, mainAppFactory, toolsFactory, eventHandler, isyTranslateFactory, $location, mainMenuPanelFactory, localStorageFactory, $translate){
+    .controller('mainAppController', ['$scope','ISY.MapAPI.Map','mainAppFactory','toolsFactory','ISY.EventHandler','isyTranslateFactory','$location','mainMenuPanelFactory', 'localStorageFactory','$translate','$timeout',
+        function($scope, map, mainAppFactory, toolsFactory, eventHandler, isyTranslateFactory, $location, mainMenuPanelFactory, localStorageFactory, $translate, $timeout){
 
             function _initToolbar() {
                 toolsFactory.initToolbar();
@@ -15,18 +15,31 @@ angular.module('mainApp')
             };
 
             function _viewChanged(obj) {
-                $scope.$apply(function () {
-                    _setSearch(obj);
-                },0);
                 localStorageFactory.set("lat", $location.search().lat);
                 localStorageFactory.set("lon", $location.search().lon);
                 localStorageFactory.set("zoom", $location.search().zoom);
+                $timeout(function() {
+                    $scope.$apply(function () {
+                        _setSearch(obj);
+                    }, 0);
+                },10);
+            }
+
+            function _loadingLayerEnd(obj) {
+                localStorageFactory.set("lat", $location.search().lat);
+                localStorageFactory.set("lon", $location.search().lon);
+                localStorageFactory.set("zoom", $location.search().zoom);
+                $timeout(function() {
+                    $scope.$apply(function () {
+                        _setSearch(obj);
+                    },0);
+                },5);
             }
 
             function _registerEvents(){
                 eventHandler.RegisterEvent(ISY.Events.EventTypes.MapConfigLoaded, _initToolbar);
                 eventHandler.RegisterEvent(ISY.Events.EventTypes.MapMoveend, _viewChanged);
-                // eventHandler.RegisterEvent(ISY.Events.EventTypes.ChangeLayers, _changedLayers);
+                eventHandler.RegisterEvent(ISY.Events.EventTypes.ChangeLayers, _loadingLayerEnd);
             }
 
             function _initUrl() {
@@ -41,11 +54,16 @@ angular.module('mainApp')
                         map.SetCenter(center);
                     }
                 }
+
+                if (obj.layers !== undefined){
+                    mainAppFactory.setInitLayersInUrl(obj.layers);
+                }
                 var newSearch = angular.extend($location.search(), obj);
                 $location.search(newSearch);
             }
 
             $scope.initMapLayout = function(){
+                _initActiveLanguage();
                 var obj = $location.search();
                 if (obj.type !== undefined){
                     if (obj.type === "1"){
@@ -65,6 +83,28 @@ angular.module('mainApp')
                 }
             }
 
+            function _initMapLayers() {
+                var mapLayers = mainAppFactory.getInitLayersInUrl();
+                if (mapLayers !== undefined){
+                    var layers = mapLayers.split(",");
+                    var overlayLayers = map.GetOverlayLayers();
+                    var baseLayers = map.GetBaseLayers();
+                    for (var i = 0; i < layers.length; i++){
+                        for (var j = 0; j < overlayLayers.length; j++){
+                            if (parseInt(layers[i], 10) === overlayLayers[j].id){
+                                overlayLayers[j].isVisible = true;
+                                map.ShowLayer(overlayLayers[j]);
+                            }
+                        }
+                        for (var m = 0; m < baseLayers.length; m++){
+                            if (parseInt(layers[i], 10) === baseLayers[m].id){
+                                map.SetBaseLayer(baseLayers[m]);
+                            }
+                        }
+                    }
+                }
+            }
+
             $scope.initMainPage = function () {
                 _registerEvents();
                 map.SetTranslateOptions(isyTranslateFactory.getTranslateOptionsByActiveLanguage());
@@ -73,12 +113,11 @@ angular.module('mainApp')
                 var mapConfig = mainAppFactory.getMapConfig();
                 map.Init('mapDiv', mapConfig);
                 _initUrl();
+                _initMapLayers();
             };
 
             $( document ).ready(function() {
-                _initActiveLanguage();
                 $scope.initMainPage();
-
             });
 
             $scope.showMapLayout = function () {
@@ -89,7 +128,7 @@ angular.module('mainApp')
                 $scope.mapTypeLayout = "mapOverlaysLayout";
             };
 
-            // $scope.mapTypeLayout = "mapOverlaysLayout";
+            $scope.mapTypeLayout = "mapOverlaysLayout";
 
         }
     ]);
