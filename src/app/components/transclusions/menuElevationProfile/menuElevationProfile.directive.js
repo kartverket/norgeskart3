21b4cecb57323243a4d13d945a6f9381aae3d8ee
jarle pedersen <jarle.pedersen@norconsult.com>
2016-11-10 13:04:53 +0100

    draw elevationProfile

--- src/app/components/transclusions/menuElevationProfile/menuElevationProfile.directive.js
+++ src/app/components/transclusions/menuElevationProfile/menuElevationProfile.directive.js
@@ -1,11 +1,46 @@
 angular.module('menuElevationProfile')
-    .directive('menuElevationProfile', [
-        function() {
+    .directive('menuElevationProfile', ['toolsFactory',
+        function(toolsFactory) {
             return {
                 templateUrl: 'components/transclusions/menuElevationProfile/menuElevationProfile.html',
                 restrict: 'A',
-                link: function(){
+                link: function(scope){
+                    var _elevationProfileActive = false;
 
+                    /*
+                     Drawing tools start
+                     */
+                    function _startDrawing (style) {
+                        var addFeatureTool = toolsFactory.getToolById("AddLayerFeature");
+                        toolsFactory.setAddFeatureType(style, "AddLayerFeature");
+                        toolsFactory.activateTool(addFeatureTool);
+                    }
+                    /*
+                     Drawing tools end
+                     */
+
+
+                    /*
+                     Calculate elevation profile start
+                     */
+
+                    scope.drawLineElevation = function(){
+                        _elevationProfileActive = true;
+                        scope.elevationImage = undefined;
+                        _startDrawing("Line");
+                    };
+
+                    scope.calculateElevationProfile = function () {
+                        if (!_elevationProfileActive){
+                            toolsElevationProfileFactory.loadXmlFile();
+                        }
+                        toolsElevationProfileFactory.generateElevationProfile();
+                        scope.elevationImage = toolsElevationProfileFactory.getElevationImage();
+                        _elevationProfileActive = false;
+                    };
+                    /*
+                     Calculate elevation profile end
+                     */
                 }
             };
         }]);
