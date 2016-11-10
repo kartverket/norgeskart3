f3248264ce9810b0e64af2bef71e7017b00c35ea
jarle pedersen <jarle.pedersen@norconsult.com>
2016-11-10 10:12:43 +0100

    content

---
+++ src/app/components/transclusions/menuElevationProfile/menuElevationProfile.directive.js
@@ -0,0 +1,11 @@
+angular.module('menuElevationProfile')
+    .directive('menuElevationProfile', [
+        function() {
+            return {
+                templateUrl: 'components/transclusions/menuElevationProfile/menuElevationProfile.html',
+                restrict: 'A',
+                link: function(){
+
+                }
+            };
+        }]);
