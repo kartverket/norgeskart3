25beb88f7af6dcc64be45af98b665f8241aa05c2
jarle pedersen <jarle.pedersen@norconsult.com>
2016-11-18 11:09:16 +0100

    temporary check-in

---
+++ src/app/components/transclusions/menuElevationProfile/menuElevationProfilePage2/menuElevationProfilePage2.controller.js
@@ -0,0 +1,13 @@
+angular.module('menuElevationProfilePage2')
+    .controller('menuElevationProfilePage2Controller', [ '$scope','toolsElevationProfileFactory',
+        function(scope, toolsElevationProfileFactory) {
+            scope.viewElevationProfile = function () {
+                scope.elevationImage = toolsElevationProfileFactory.getElevationImage();
+            };
+
+            scope.getElevationImage = function () {
+              return scope.elevationImage;
+            };
+
+        }
+    ]);
