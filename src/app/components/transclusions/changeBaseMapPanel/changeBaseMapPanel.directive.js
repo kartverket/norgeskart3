56fa80782346d25f8143a29121576c2022320a6e
jucir <juraj.cirbus@norconsutl.com>
2016-12-13 16:07:38 +0100

    change base map

--- src/app/components/transclusions/changeBaseMapPanel/changeBaseMapPanel.directive.js
+++ src/app/components/transclusions/changeBaseMapPanel/changeBaseMapPanel.directive.js
@@ -1,6 +1,6 @@
 angular.module('changeBaseMapPanel')
-    .directive('changeBaseMapPanel', ['changeBaseMapPanelFactory',
-        function(changeBaseMapPanelFactory) {
+    .directive('changeBaseMapPanel', ['changeBaseMapPanelFactory','ISY.MapAPI.Map',
+        function(changeBaseMapPanelFactory, map) {
             return {
                 templateUrl: 'components/transclusions/changeBaseMapPanel/changeBaseMapPanel.html',
                 restrict: 'A',
@@ -12,11 +12,27 @@ angular.module('changeBaseMapPanel')
 
                     scope.toggleBaseMap = function (id) {
                         changeBaseMapPanelFactory.setBaseMapById(id);
+                        var baseMapLayers = map.GetBaseLayers();
+                        for (var i = 0; i < baseMapLayers.length; i++){
+                            if (baseMapLayers[i].thumbnail === id){
+                                map.SetBaseLayer(baseMapLayers[i]);
+                            }
+                        }
                     };
 
                     scope.activeBaseMap = function () {
                         return changeBaseMapPanelFactory.getSelectedBaseMap();
                     };
+
+                    $(document).ready(function() {
+                        var baseMapLayers = map.GetBaseLayers();
+                        for (var i = 0; i < baseMapLayers.length; i++){
+                            if (baseMapLayers[i].thumbnail !== '' && baseMapLayers[i].isVisible){
+                                changeBaseMapPanelFactory.setBaseMapById(baseMapLayers[i].thumbnail);
+                            }
+                        }
+
+                    });
 
                 }
             };
