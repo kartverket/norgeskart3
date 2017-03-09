0f7a020ae6f4ac76c16cbde86bdf830ad21b9eea
Carsten Mielke <carsten.mielke@gmail.com>
2017-03-09 12:02:59 +0100

    use vendor minified, cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -646,7 +646,7 @@ module.exports = function ( grunt ) {
         'copy:compile_assets',
         'ngmin',
         'concat:compile_js',
-        // 'uglify',
+        'uglify',
         'appcache',
         'index:compile'
     ]);
