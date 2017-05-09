49e7bbc809b2b196c24fde8d139f0ac6952ba505
havardvidme <havard.vidme@gmail.com>
2017-05-09 14:26:54 +0200

    Added 'exit embed' button to embedded maps

---
+++ src/app/shared/mapLayout/mapLayout.controller.js
@@ -0,0 +1,9 @@
+angular.module('mapLayout')
+.controller('mapLayoutController', ['$scope',
+  function($scope){
+    $scope.disableEmbed = function () {
+      var URL = location.href.replace(/[\?|\&]type\=[0-9]+/, '');
+      window.open(URL, '_blank');
+    };
+  }
+]);
