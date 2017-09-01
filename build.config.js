7401e6440a5e0223ff184f8104eea4d11ccb4537
carsmie <carsten.mielke@kartverket.no>
2017-09-01 13:54:55 +0200

    Quickfix for extent error

--- build.config.js
+++ build.config.js
@@ -78,7 +78,7 @@ module.exports = {
             'vendor/proj4/dist/proj4.js',
             'vendor/es5-shim/es5-shim.min.js',
             'vendor/pouchdb/dist/pouchdb.min.js',
-            'vendor/openlayers3/ol.js',
+            'vendor/openlayers3/ol-debug.js',
             'vendor/maptiks-ol3.min.js',
             'vendor/maptiks.js',
             'vendor/blob-util/dist/blob-util.min.js',
