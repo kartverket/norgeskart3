e2bf4d917c436ea05814ad88767557b9cbce8800
Carsten Mielke <carsten.mielke@gmail.com>
2018-05-05 00:14:17 +0200

    grey base layer chooser

--- src/app/components/transclusions/changeBaseLayerPanel/changeBaseLayerPanel.factory.js
+++ src/app/components/transclusions/changeBaseLayerPanel/changeBaseLayerPanel.factory.js
@@ -1,56 +1,56 @@
 angular
-    .module('changeBaseLayerPanel')
-    .factory('changeBaseLayerPanelFactory', [
-        function() {
+  .module('changeBaseLayerPanel')
+  .factory('changeBaseLayerPanelFactory', [
+    function () {
+      var baseLayers = [{
+          id: "land",
+          title: "Land",
+          symbol: "baseMap-land",
+          isSelected: false
+        },
+        {
+          id: "aerial",
+          title: "Flybilder",
+          symbol: "baseMap-aerial",
+          isSelected: false
+        },
+        {
+          id: "grey",
+          title: "Graatone",
+          symbol: "baseMap-grey",
+          isSelected: false
+        }
+      ];
+      return {
+        getAllBaseLayers: function () {
+          return baseLayers;
+        },
 
-            var baseLayers = [
-                {
-                    id: "land",
-                    title: "Land",
-                    symbol: "baseMap-land",
-                    isSelected : false
-                },
-                {
-                    id: "aerial",
-                    title: "Flybilder",
-                    symbol: "baseMap-aerial",
-                    isSelected : false
-                }
-            ];
+        getSelectedBaseLayer: function () {
+          for (var i = 0; i < baseLayers.length; i++) {
+            if (baseLayers[i].isSelected) {
+              return baseLayers[i];
+            }
+          }
+          return undefined;
+        },
 
-            return {
+        setBaseLayerById: function (id) {
+          if (id !== undefined) {
+            for (var i = 0; i < baseLayers.length; i++) {
+              baseLayers[i].isSelected = baseLayers[i].id.toLowerCase() === id.toLowerCase();
+            }
+          } else {
+            this.deactivateAllBaseLayers();
+          }
 
+        },
 
-                getAllBaseLayers: function(){
-                    return baseLayers;
-                },
-
-                getSelectedBaseLayer: function () {
-                    for (var i = 0; i < baseLayers.length; i++){
-                        if (baseLayers[i].isSelected){
-                            return baseLayers[i];
-                        }
-                    }
-                    return undefined;
-                },
-
-                setBaseLayerById: function (id) {
-                    if (id !== undefined){
-                        for (var i = 0; i < baseLayers.length; i++){
-                            baseLayers[i].isSelected = baseLayers[i].id.toLowerCase() === id.toLowerCase();
-                        }
-                    }else{
-                        this.deactivateAllBaseLayers();
-                    }
-
-                },
-
-                deactivateAllBaseLayers: function () {
-                    for (var i = 0; i < baseLayers.length; i++){
-                        baseLayers[i].isSelected = false;
-                    }
-                }
-            };
-
-
-        }]);
+        deactivateAllBaseLayers: function () {
+          for (var i = 0; i < baseLayers.length; i++) {
+            baseLayers[i].isSelected = false;
+          }
+        }
+      };
+    }
+  ]);
