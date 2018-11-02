f7a354f50a054363dd690202e0811d1e594fccec
Carsten Mielke <carsten.mielke@kartverket.no>
2018-11-02 09:52:34 +0100

    Lagt til et parameter for å gjemme hele menuen

--- src/app/shared/mapOverlaysLayout/mapOverlaysLayout.controller.js
+++ src/app/shared/mapOverlaysLayout/mapOverlaysLayout.controller.js
@@ -1,12 +1,14 @@
 angular.module('mapOverlaysLayout')
-  .controller('mapOverlaysLayoutController', ['$scope', 'mapOverlaysLayoutFactory',
-    function ($scope, mapOverlaysLayoutFactory) {
+  .controller('mapOverlaysLayoutController', ['$scope', 'mapOverlaysLayoutFactory', '$location',
+    function ($scope, mapOverlaysLayoutFactory, $location) {
 
       $scope.showSearchOverlay = function () {
         return mapOverlaysLayoutFactory.getShowSearchOverlay();
       };
 
       $scope.isGeolocationDisabled = 'geolocation' in navigator === false || location.protocol !== 'https:';
+
+      $scope.isModal = "modal" in $location.search() === true;
 
       $scope.getGeolocation = function () {
         navigator.geolocation.getCurrentPosition(mapOverlaysLayoutFactory.currentPosition);
