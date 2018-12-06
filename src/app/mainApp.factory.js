385fd5ad3b758996c257ff4614b0c5110cc3cbd6
Carsten Mielke <carsten.mielke@kartverket.no>
2018-12-06 13:56:08 +0100

    Ikke vis utgåtte bygg

--- src/app/mainApp.factory.js
+++ src/app/mainApp.factory.js
@@ -322,7 +322,7 @@ angular.module('mainApp')
             type: "overlay",
             gatekeeper: "true",
             name: "bygninger",
-            url: "//wms.geonorge.no/skwms1/wms.matrikkel.seeiendom2",
+            url: "//wms.geonorge.no/skwms1/wms.matrikkel.seeiendom2/?CQL_FILTER=BYGNINGSTATUS<9",
             groupid: 1,
             maxResolution: 2,
             params: {
