a92ff970a6f3d4ecb8908ba171fef39eb8efb142
jucir <juraj.cirbus@norconsult.com>
2018-12-11 09:44:06 +0100

    - fixed nynorsk

--- src/app/shared/isyTranslate/isyTranslate.factory.js
+++ src/app/shared/isyTranslate/isyTranslate.factory.js
@@ -47,6 +47,20 @@ angular
         start_measure_line: "Klikk for å starte tegning",
         continue_measure_line: "Klikk for å fortsette å tegne linjen. Klikk i siste punktet for å avslutte linjen."
       },
+      nn: {
+        east: "&Oslash; ",
+        north: "N ",
+        start_modify: "Velg objekt og starte redigering.",
+        add_layer_start_drawing: "Klikk for å starte tegning",
+        add_layer_continue_drawing: "Klikk for å fortsette å tegne.",
+        add_layer_modify_object: "Velg objekt og starte redigering.",
+        start_drawing: "Klikk for å starte tegning",
+        continue_drawing: "Klikk for å fortsette å tegne polygon. Klikk i siste punktet for å avslutte polygon.",
+        start_measure: "Klikk for å starte tegning",
+        continue_measure: "Klikk for å fortsette å tegne polygon. Klikk i siste punktet for å avslutte polygon.",
+        start_measure_line: "Klikk for å starte tegning",
+        continue_measure_line: "Klikk for å fortsette å tegne linjen. Klikk i siste punktet for å avslutte linjen."
+      },
       en: {
         east: "E ",
         north: "N ",
