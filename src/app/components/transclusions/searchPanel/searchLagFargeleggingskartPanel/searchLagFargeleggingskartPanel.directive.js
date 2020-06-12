0e3f18d20faaeab96544a176a9a7513db1cd83f3
Carsten Mielke <carsten.mielke@kartverket.no>
2020-06-12 16:19:44 +0200

    Add type 'fargelegg'

--- src/app/components/transclusions/searchPanel/searchLagFargeleggingskartPanel/searchLagFargeleggingskartPanel.directive.js
+++ src/app/components/transclusions/searchPanel/searchLagFargeleggingskartPanel/searchLagFargeleggingskartPanel.directive.js
@@ -81,7 +81,8 @@ angular.module('searchLagFargeleggingskartPanel')
               layout: "A4 landscape",
               scale: extent.scale,
               titel: scope.tittel,
-              link: "http://www.norgeskart.no/#!?zoom=" + scope.activePosition.zoom + "&lat=" + Number(Math.round(scope.activePosition.lat + 'e' + 2) + 'e-' + 2) + "&lon=" + Number(Math.round(scope.activePosition.lon + 'e' + 2) + 'e-' + 2)
+              link: "http://www.norgeskart.no/#!?zoom=" + scope.activePosition.zoom + "&lat=" + Number(Math.round(scope.activePosition.lat + 'e' + 2) + 'e-' + 2) + "&lon=" + Number(Math.round(scope.activePosition.lon + 'e' + 2) + 'e-' + 2),
+              type: "fargelegg"
             };
           };
 
