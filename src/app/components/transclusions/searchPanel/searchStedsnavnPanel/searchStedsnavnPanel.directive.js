9517a94b65870e5f1abc81c578f1a88d3fb1ffd0
Carsten Mielke <carsten.mielke@kartverket.no>
2021-11-03 13:49:33 +0100

    fix scroll issue

--- src/app/components/transclusions/searchPanel/searchStedsnavnPanel/searchStedsnavnPanel.directive.js
+++ src/app/components/transclusions/searchPanel/searchStedsnavnPanel/searchStedsnavnPanel.directive.js
@@ -13,7 +13,34 @@ angular.module('searchStedsnavnPanel')
             map.RemoveInfoMarker();
             map.ShowInfoMarker(coord);
           };
+          var setMenuListMaxHeight = function () {
+            $(document).ready(function () {
+              var isMobile = $window.matchMedia("only screen and (max-width: 760px)");
+              if (isMobile.matches) {
+                fixElementHeight(120);
+              } else {
+                fixElementHeight(220);
+              }
+            });
+          };
 
+          function fixElementHeight(moveUpFromBottom) {
+            var bodyHeight = $window.innerHeight;
+            var menuListMaxHeight = Math.floor(bodyHeight - moveUpFromBottom);
+            var searchContentElements = document.getElementsByClassName("search-content");
+            for (var i = 0; i < searchContentElements.length; i++) {
+              var element = searchContentElements[i];
+              element.style.maxHeight = menuListMaxHeight + 'px';
+            }
+          }
+
+          scope.isIosDevice = function () {
+            return !!(navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i));
+          };
+          $(document).ready(function () {
+            $($window).resize(setMenuListMaxHeight);
+            setMenuListMaxHeight();
+          });
         }
       }
     }
