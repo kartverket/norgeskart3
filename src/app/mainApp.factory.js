9928533f19cdeb767a6ab75fa6edc6d4dd1b6985
carsmie <carsten.mielke@kartverket.no>
2025-04-04 08:50:20 +0200

    Bump version to 2.0.100 and update WMTS service URLs
    - Updated package version in package.json from 2.0.99 to 2.0.100.
    - Changed WMTS service URLs in mainApp.factory.js to point to the new internal cloud service.

--- src/app/mainApp.factory.js
+++ src/app/mainApp.factory.js
@@ -128,7 +128,7 @@ angular.module('mainApp')
           wmts: [{
             type: "map",
             name: "landkart",
-            url: "https://cache.kartverket.no/v1/wmts|https://cache.kartverket.no/v1/wmts",
+            url: "https://cache.atkv3-dev.kartverket-intern.cloud/v1/service",
             params: {
               layers: "topo",
               format: "image/png"
@@ -1639,7 +1639,7 @@ angular.module('mainApp')
               wmts: [{
                 type: "map",
                 name: "landkart",
-                url: "https://cache.kartverket.no/v1/wmts|https://cache.kartverket.no/v1/wmts",
+                url: "https://cache.atkv3-dev.kartverket-intern.cloud/v1/service",
                 params: {
                   layers: "topo",
                   format: "image/png"
